cc30ff5afa1ca5019abd764b6954b2fdf670dd06
Mikhail Popov <mikh.popov@vk.team>
2022-07-10 22:46:44 +0300

    fix mailer

--- internal/pkg/mailer/mailer.go
+++ internal/pkg/mailer/mailer.go
@@ -8,8 +8,8 @@ import (
 func SendVerifiedEmail(to_email string) error {
 	msg := gomail.NewMessage()
 	msg.SetHeader("From", config.Mailer.Email)
-	msg.SetHeader("To", "<paste the email address you want to send to>")
-	msg.SetHeader("Subject", to_email)
+	msg.SetHeader("To", to_email)
+	msg.SetHeader("Subject", "prikol")
 	msg.SetBody("text/html", "<b>This is the body of the mail</b>")
 
 	n := gomail.NewDialer("smtp.gmail.com", 587, config.Mailer.Email, config.Mailer.Password)
